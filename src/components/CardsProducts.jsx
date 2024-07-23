3cf229866204d8f94566ac940fa9730f88cf38be
MayraCortinez <mayrautopia@gmail.com>
2024-07-23 11:30:29 -0300

    first commit

---
+++ src/components/CardsProducts.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function CardsProducts() {
+  return (
+    <div class="hexagon">
+    <div class="hexagon-content">
+      <p class="text-sm font-semibold">Card 1</p>
+    </div>
+  </div>
+  )
+}
+
+export default CardsProducts;
